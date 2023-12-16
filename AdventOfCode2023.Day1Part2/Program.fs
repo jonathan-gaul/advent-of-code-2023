3d96580ab0d026cf399e54b356c3c4ac11816fa5
Jon Gaul <jon@gaul.tech>
2023-12-16 13:21:45 +0000

    Day 1 task working

---
+++ AdventOfCode2023.Day1Part2/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let lines = System.IO.File.ReadLines("input.txt")
+
+let total =
+    lines 
+    |> Seq.map (fun s ->
+        let digits = s |> Seq.where System.Char.IsDigit
+        let first = digits |> Seq.head
+        let last = digits |> Seq.last
+
+        int (first - '0') * 10 + int (last - '0'))
+    |> Seq.sum
+
+total |> printf "%d"
