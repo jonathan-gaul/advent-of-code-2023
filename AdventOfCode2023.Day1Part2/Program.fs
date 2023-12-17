646bb4bb2788d5bc9fa7d75433ea7d1e6cf152e4
Jon Gaul <jon@gaul.tech>
2023-12-17 01:08:59 +0000

    Day 1 Part 2 working

--- AdventOfCode2023.Day1Part2/Program.fs
+++ AdventOfCode2023.Day1Part2/Program.fs
@@ -2,14 +2,35 @@
 
 let lines = System.IO.File.ReadLines("input.txt")
 
+let numbers = [ "zero"; "0"; "one"; "1"; "two"; "2"; "three"; "3"; "four"; "4"; "five"; "5"; "six"; "6"; "seven"; "7"; "eight"; "8"; "nine"; "9" ]
+
+let expr = 
+    numbers 
+    |> fun s -> $"(?=.*?({s}))?"
+    |> (+) "^"
+
 let total =
     lines 
     |> Seq.map (fun s ->
-        let digits = s |> Seq.where System.Char.IsDigit
-        let first = digits |> Seq.head
-        let last = digits |> Seq.last
+        let first = 
+            numbers            
+            |> List.mapi (fun i num -> ((i + 2) / 2 - 1, s.IndexOf(num)))
+            |> List.filter (fun (_, p) -> p >= 0)
+            |> List.sortBy snd
+            |> List.head
+            |> fst
 
-        int (first - '0') * 10 + int (last - '0'))
+        let last = 
+            numbers 
+            |> List.mapi (fun i num -> ((i + 2) / 2 - 1, s.LastIndexOf(num)))
+            |> List.filter (fun (_, p) -> p >= 0)
+            |> List.sortByDescending snd
+            |> List.head
+            |> fst
+            
+        printf "%d / %d\n" first last 
+
+        first * 10 + last)
     |> Seq.sum
 
 total |> printf "%d"
